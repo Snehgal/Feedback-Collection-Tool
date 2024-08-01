d8a2db7666789787adf8e11adf2ddbc592a546ac
Chirag Sehgal <chirag23176@iiitd.ac.in>
2024-08-02 02:28:50 +0530

    updated package.json files

--- Method3_MongoDBmethod/controlESP8266/testWS.js
+++ Method3_MongoDBmethod/controlESP8266/testWS.js
@@ -8,12 +8,28 @@ ws.on('open', () => {
 
     // Send 5 different data points
     const dataPoints = [
-        '1001\t0',  // Example data: tableID 1000, value 1
-        '2001\t1',  // Example data: tableID 2000, value 2
-        '3002\t1',  // Example data: tableID 3000, value 1
-        '4003\t1',  // Example data: tableID 1000, value 1
-        '7001\t0',  // Example data: tableID 2000, value 2
-        '7003\t1',  // Example data: tableID 3000, value 1
+        '1001\t0', 
+        '1001\t0',
+        '2001\t1',
+        '3002\t1',
+        '4003\t0',
+        '5004\t1',
+        '6001\t0',
+        '7002\t1',
+        '8003\t0',
+        '9004\t1',
+        '11001\t1',
+        '12002\t0',
+        '13003\t1',
+        '14004\t0',
+        '15001\t1',
+        '16002\t0',
+        '17003\t1',
+        '18004\t0',
+        '19001\t1',
+        '20002\t0',
+        '21003\t1',
+        '22004\t0'
     ];
 
     dataPoints.forEach((data, index) => {
